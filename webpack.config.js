45f99d544dd50645f49dcc08c8101cb8fd8970f7
Yukio Niwa <niwakk6@gmail.com>
2017-06-25 21:30:59 +0900

    add source map and build for dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,19 @@
+require('babel-polyfill');
 const path = require('path');
+const webpack = require("webpack");
+
+const DEBUG = !process.argv.includes('--env.production');
+
+const plugins = [
+  new webpack.optimize.OccurrenceOrderPlugin()
+];
+
+if(!DEBUG){
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({ compress: { screw_ie8: false, warnings: false } }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  );
+}
 
 module.exports = {
   entry: path.join(__dirname, 'src/app.js'),
@@ -11,6 +26,8 @@ module.exports = {
     port: 3000,
     inline: true
   },
+  plugins: plugins,
+  devtool: DEBUG ? 'cheap-module-eval-source-map' : false,
   module: {
     rules: [
       {
