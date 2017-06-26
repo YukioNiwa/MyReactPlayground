143a4927f1900e225b81b64aacd1dbc3f9000853
Yukio Niwa <niwakk6@gmail.com>
2017-06-26 22:34:19 +0900

    add count up and css

--- src/app.js
+++ src/app.js
@@ -2,8 +2,31 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
-  render () {
-    return <p>Hello React!</p>;
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render(){
+    var btnstyle = {
+      color: 'red',
+      backgroundColor: 'blue',
+    };
+
+    // http://qiita.com/koba04/items/0e81a04262e1158dbbe4
+    return(
+      <div>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleClick.bind(this)}
+          style={btnstyle}>+</button>
+      </div>
+    )
   }
 }
 
