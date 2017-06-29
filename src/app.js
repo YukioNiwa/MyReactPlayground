9a253e48b9848ef9d7b888c95af03c678fa195c7
Yukio Niwa <niwakk6@gmail.com>
2017-06-29 22:47:44 +0900

    add github list display

--- src/app.js
+++ src/app.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+
+const REQUEST_URL = 'https://api.github.com/users/niwakk7/repos'
 
 class App extends React.Component {
-  render () {
-    return <p>Hello React!</p>;
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    // https://github.com/github/fetch
+    // 無名関数
+    // http://unitopi.com/js-function/
+
+    //fetch
+    //http://var.blog.jp/archives/52758828.html
+
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          data: responseData,
+        })
+    })
+  }
+
+  render(){
+    return(
+      <ul>
+        {this.state.data.map((item) => {
+          return (
+            <li key={item.id}>{item.name}</li>
+          )
+        })}
+      </ul>
+    )
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(
+  <App />,
+  document.getElementById('app')
+)
